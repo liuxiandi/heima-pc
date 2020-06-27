c7b0e49bb0080593cbda5f11cafb22bfc6781c7e
liuxiandi <liuxiandi@example.com>
2020-06-27 17:09:48 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 // 引入element-UI样式
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
 Vue.config.productionTip = false
 // vue.use相当于全局注册 一旦全局注册任何位置都可与使用组件 Vue.use实际上会调用element-ui对象
 // 里面的一个install方法 并且传入当前的vue 对象

ea99edd1a85b80d0bdb34fcebf867f33dc362e17
liuxiandi <liuxiandi@example.com>
2020-08-03 09:28:50 +0800

    面包屑组件的封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-breadcrumb>
+      <el-breadcrumb-item separator=">">首页</el-breadcrumb-item>
+      <!-- 第二个选项是动态的 需要采用传值的方式进来 props传值 这里采用slot插槽 -->
+       <el-breadcrumb-item>
+           <!-- 内容是动态的 定义一个具名插槽 -->
+           <slot name='title'></slot>
+       </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -12,11 +12,13 @@
 // vue.use会调用对象里面的install方法 install方法的第一个参数是Vue对象
 import LayoutHeader from './home/layout-header'
 import LayoutAside from './home/layout-aside'
+import Breadcrumb from './common/bread-crumb'
 
 export default {
   install: function (Vue) {
     //   注册全局组件 Vue 一旦注册 在任意位置都可以使用
     Vue.component('layout-header', LayoutHeader) // 注册头部组件
     Vue.component('layout-aside', LayoutAside) // 注册左侧导航组件
+    Vue.component('bread-crumb', Breadcrumb)
   }
 }

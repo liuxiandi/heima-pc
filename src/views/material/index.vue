01a97f0566d9020b116ce54bf0aa847d6559280d
liuxiandi <liuxiandi@example.com>
2020-08-07 17:52:14 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -8,9 +8,26 @@
     </bread-crumb>
 
     <!-- 放置标签页 -->
-    <el-tabs v-model="activeName">
-      <el-tab-pane label="全部素材" name="all">全部素材</el-tab-pane>
-      <el-tab-pane label="收藏素材" name="second">收藏素材</el-tab-pane>
+    <el-tabs v-model="activeName" @tab-click="changeTap">
+      <el-tab-pane label="全部素材" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url">
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+
+      <el-tab-pane label="收藏素材" name="second">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url">
+          </el-card>
+        </div>
+        </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -20,21 +37,27 @@ export default {
   data () {
     return {
       activeName: 'all',
-      //   定义一个数组 接受全部素材的数据
+      //   定义一个数组 接受全部素材的数据 和 收藏素材数据
       list: []
     }
   },
   methods: {
+    // 切换页签事件
+    changeTap () {
+      // 根据当前activeName 来决定是获取的那个数据
+      this.getMaterial()
+    },
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: false
+          // 如果this.activeName === 'second' 那么为true 获取收藏的数据  如果点击的是全部数据那么this.activeName === 'second'为false 获取全部的数据
+          collect: this.activeName === 'second'
         },
         //   data参数放的是body参数
         data: {}
-      }).then(result => {
-
+      }).then((result) => {
+        this.list = result.data.results
       })
     }
   },
@@ -44,5 +67,31 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+  .img-list {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      .img-card {
+          width: 200px;
+          height: 220px;
+          margin: 20px 10px;
+          position: relative;
+          img {
+              width: 100%;
+              height: 100%;
+          }
+          .operate {
+              position: absolute;
+              left:0;
+              bottom:0;
+              width: 100%;
+              background: #f4f5f6;
+              height: 30px;
+              i  {
+                  font-size:20px;
+              }
+          }
+      }
+  }
 </style>

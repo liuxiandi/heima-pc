fc7bdc511b4173929965a26e25be7655c8cb4452
liuxiandi <liuxiandi@example.com>
2020-08-09 23:04:11 +0800

    内容列表-新建页面-挂载路由

---
+++ src/views/articles/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card>
+    <!-- 放置面包屑  将 面包屑给到卡片的具名header-->
+    <bread-crumb slot="header">
+      <!-- 具名插槽 template不生成任何实质标签-->
+      <template slot="title">内容列表</template>
+    </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>

36d17bb5f44a3936f326e648759af60d29138c22
liuxiandi <liuxiandi@example.com>
2020-08-10 16:27:29 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -5,11 +5,64 @@
       <!-- 具名插槽 template不生成任何实质标签-->
       <template slot="title">内容列表</template>
     </bread-crumb>
+
+    <el-form>
+      <el-form-item label="文章状态：">
+        <el-radio-group v-model="searchForm.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="频道类型:">
+        <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="日期范围:">
+        <el-date-picker
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          v-model="searchForm.dateRange"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      searchForm: {
+        // 数据
+        // 文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部 / 先将 5 定义成 全部
+        // 传值的是时候如果是5 就不传
+        status: 5, // 默认为全部的状态
+        channel_id: null // 表示没有任何的频道
+      },
+      channels: [],
+      dateRange: []
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then((result) => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
+}
 </script>
 
 <style>
